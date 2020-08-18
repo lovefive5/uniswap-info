4074aef2ea887ad27daef2ae15caaf49a707edfb
auspicious <18437980785@163.com>
2020-08-18 14:19:42 +0800

    update logo path

--- src/components/TokenLogo/index.js
+++ src/components/TokenLogo/index.js
@@ -66,7 +66,7 @@ export default function TokenLogo({ address, header = false, size = '24px', ...r
     )
   }
 
-  const path = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${isAddress(
+  const path = `https://ethapi.meet.one/uniswap/trustwallet/assets/master/blockchains/ethereum/assets/${isAddress(
     address
   )}/logo.png`
 
